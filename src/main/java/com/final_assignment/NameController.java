7a826836124c8a77e79f43205cd1fa0a5b9a7031
Rikiya <d4c.raise.ability@gmail.com>
2024-04-18 00:23:14 +0900

    read処理を作成

---
+++ src/main/java/com/final_assignment/NameController.java
@@ -0,0 +1,42 @@
+package com.final_assignment;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class NameController {
+    private final NameService nameService;
+
+    public NameController(NameService nameService) {
+        this.nameService = nameService;
+    }
+
+    @GetMapping("/names")
+    public List<Name> findByNames(@RequestParam (required = false) String startsWith) {
+        return nameService.findByNameStartingWith(startsWith);
+    }
+
+
+    @GetMapping("/names/{id}")
+    public Name findName(@PathVariable("id") int id) {
+        return nameService.findName(id);
+    }
+
+    @ExceptionHandler(value = NameNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNameNotFoundException(
+            NameNotFoundException e, HttpServletRequest request) {
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "message", e.getMessage(),
+                "path", request.getRequestURI());
+        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/final_assignment/NameNotFoundException.java
@@ -0,0 +1,8 @@
+package com.final_assignment;
+
+public class NameNotFoundException extends RuntimeException{
+    public NameNotFoundException(String message) {
+        super(message);
+    }
+}
+
