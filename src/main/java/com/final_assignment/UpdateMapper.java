c92fb28e8e38013edd8c0240027e737d0dff6c79
Rikiya <d4c.raise.ability@gmail.com>
2024-05-04 12:15:57 +0900

    UpdateMapperのエラーについて質問

--- src/main/java/com/final_assignment/UpdateMapper.java
+++ src/main/java/com/final_assignment/UpdateMapper.java
@@ -1,2 +1,10 @@
-package com.final_assignment;public interface UpdateMapper {
+package com.final_assignment;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface UpdateMapper {
+    @Update("UPDATE users SET name = #{name}, email = #{email} WHERE id = #{id}")
+    static void updateEntity(Entity entity);
 }
--- src/main/java/com/final_assignment/UpdateService.java
+++ src/main/java/com/final_assignment/UpdateService.java
@@ -1,2 +1,19 @@
-package com.final_assignment;public class UpdateService {
+package com.final_assignment;
+
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateService {
+    private Update update;
+
+    public UpdateService(Update update) {
+        this.update = update;
+    }
+
+    public Entity updateEntity(String name, String email) {
+        Entity entity = new Entity(null, name, email);
+        UpdateMapper.updateEntity(entity);
+        return entity;
+    }
 }
