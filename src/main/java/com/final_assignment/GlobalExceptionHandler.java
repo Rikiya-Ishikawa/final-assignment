bd9cb97114dce544731159b59737038de0a4d4d7
Rikiya <d4c.raise.ability@gmail.com>
2024-05-25 23:12:27 +0900

    更新処理作成完了

--- src/main/java/com/final_assignment/GlobalExceptionHandler.java
+++ src/main/java/com/final_assignment/GlobalExceptionHandler.java
@@ -3,31 +3,14 @@ package com.final_assignment;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.ZonedDateTime;
-import java.util.List;
 import java.util.Map;
 
-@RestController
-public class NameController {
-    private final NameService nameService;
-
-    public NameController(NameService nameService) {
-        this.nameService = nameService;
-    }
-
-    @GetMapping("/names")
-    public List<Name> findByNames(@RequestParam (required = false) String startsWith) {
-        return nameService.findByNameStartingWith(startsWith);
-    }
-
-
-    @GetMapping("/names/{id}")
-    public Name findName(@PathVariable("id") int id) {
-        return nameService.findName(id);
-    }
-
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    // NameNotFoundExceptionをハンドルするメソッド
     @ExceptionHandler(value = NameNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleNameNotFoundException(
             NameNotFoundException e, HttpServletRequest request) {
@@ -37,6 +20,6 @@ public class NameController {
                 "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
                 "message", e.getMessage(),
                 "path", request.getRequestURI());
-        return new ResponseEntity(body, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/final_assignment/UserController.java
+++ src/main/java/com/final_assignment/UserController.java
@@ -1,12 +1,10 @@
 package com.final_assignment;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,12 +13,29 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/users")
+    public List<User> findByNames(@RequestParam(required = false) String startsWith) {
+        return userService.findByNameStartingWith(startsWith);
+    }
+
+
+    @GetMapping("/users/{id}")
+    public User findName(@PathVariable("id") int id) {
+        return userService.findName(id);
+    }
     @PostMapping("/users")
     public ResponseEntity<UserResponse> insert(@RequestBody UserRequest userRequest, UriComponentsBuilder uriBuilder) {
         User user = userService.insert(userRequest.getName(), userRequest.getEmail());
         URI location = uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
         UserResponse body = new UserResponse("user created");
         return ResponseEntity.created(location).body(body);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable Integer id, @RequestBody UserRequest request) {
+        userService.update(id, request.getName(), request.getEmail());
+        UserResponse response = new UserResponse("user updated");
+        return ResponseEntity.ok(response);
     }
 
 }
